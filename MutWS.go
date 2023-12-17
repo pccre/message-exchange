fccd08f31a8a54cffc5aaadb1ac95ea6a2b3ebd7
Laptop <laptop@disroot.org>
2023-12-17 11:38:21 +0200

    Update MutWS.go

--- MutWS.go
+++ MutWS.go
@@ -1,22 +1,26 @@
 package main
 
 import (
-	"sync"
+  "sync"
 
-	"github.com/gofiber/contrib/websocket"
+  "github.com/gofiber/contrib/websocket"
 )
 
 type MutWS struct {
-	WS  *websocket.Conn
-	Mut *sync.Mutex
+  WS  *websocket.Conn
+  Mut *sync.Mutex
 }
 
 func (c *MutWS) WriteJSON(content interface{}) error {
-	data, err := json.Marshal(content)
-	if err != nil {
-		return err
-	}
-	c.Mut.Lock()
-	defer c.Mut.Unlock()
-	return c.WS.WriteMessage(1, data)
+  data, err := json.Marshal(content)
+  if err != nil {
+    return err
+  }
+  return c.WriteRaw(data)
+}
+
+func (c *MutWS) WriteRaw(content []byte) error {
+  c.Mut.Lock()
+  defer c.Mut.Unlock()
+  return c.WS.WriteMessage(1, content)
 }
